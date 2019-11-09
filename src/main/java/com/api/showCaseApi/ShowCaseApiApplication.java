eca80a0a42ebb5f5dbe2b88c05f698e1f1d221e9
Aditya Kumar <adityak007mr@gmail.com>
2019-11-09 15:19:14 +0530

    initial commit

---
+++ src/main/java/com/api/showCaseApi/ShowCaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.showCaseApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowCaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowCaseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/showCaseApi/ShowCaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.showCaseApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowCaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
