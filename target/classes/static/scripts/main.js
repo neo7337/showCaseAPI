691b22ee30834177010609c3ae8be247e18c7f73
Aditya Kumar <adityak007mr@gmail.com>
2020-01-26 22:44:22 +0530

    new functionality added

--- target/classes/static/scripts/main.js
+++ target/classes/static/scripts/main.js
@@ -1,3 +1,4 @@
+var stop = false;
 $(document).ready(function () {
     $('#res').jQCloud([], {
         classPattern: null,
@@ -8,49 +9,51 @@ $(document).ready(function () {
         },
         width: 1100,
         height: 350
-      });
+    });
     function viewData(){
-        var data= {};
-        $.ajax({
-            type: 'GET',
-            data: data,
-            contentType: 'application/json',
-            url: '/api/v1/fetchData',
-            dataType: 'json',
-            success: (result) => {
-                $("#result").empty();
-                var items2 = [];
-                var returnMovies = JSON.stringify(result);
-                var jsonData = JSON.parse(returnMovies);                 
-                var lan = [],
-                    count = [],
-                    prev;
-                for (var key in jsonData) {
-                    lan.push(key);
-                    count.push(jsonData[key]);
+        if(!stop){
+            var data= {};
+            $.ajax({
+                type: 'GET',
+                data: data,
+                contentType: 'application/json',
+                url: '/api/v1/fetchData',
+                dataType: 'json',
+                success: (result) => {
+                    $("#result").empty();
+                    var items2 = [];
+                    var returnMovies = JSON.stringify(result);
+                    var jsonData = JSON.parse(returnMovies);                 
+                    var lan = [],
+                        count = [],
+                        prev;
+                    for (var key in jsonData) {
+                        lan.push(key);
+                        count.push(jsonData[key]);
+                    }
+                    var countSum = 0;
+                    for (var h = 0; h < count.length; h++) {
+                        countSum = countSum + count[h];
+                    }
+                    for (var g = 0; g < lan.length; g++) {
+                        items2.push('<li class="list-group-item">' + lan[g] + '<span class="badge">' + Math.round((count[g] / countSum) * 100) + ' % </span></li>');
+                    }
+                    var perc = [];
+                    for (var t = 0; t < count.length; t++) {
+                        perc[t] = Math.round((count[t] / countSum) * 100);
+                    }
+                    var data = [];
+                    for(var a=0; a<lan.length; a++){
+                        var obj = {};
+                        obj["text"]=lan[a];
+                        obj["weight"]=perc[a];
+                        data.push(obj);
+                    }
+                    $('#res').jQCloud('update', data);
                 }
-                var countSum = 0;
-                for (var h = 0; h < count.length; h++) {
-                    countSum = countSum + count[h];
-                }
-                for (var g = 0; g < lan.length; g++) {
-                    items2.push('<li class="list-group-item">' + lan[g] + '<span class="badge">' + Math.round((count[g] / countSum) * 100) + ' % </span></li>');
-                }
-                var perc = [];
-                for (var t = 0; t < count.length; t++) {
-                    perc[t] = Math.round((count[t] / countSum) * 100);
-                }
-                var data = [];
-                for(var a=0; a<lan.length; a++){
-                    var obj = {};
-                    obj["text"]=lan[a];
-                    obj["weight"]=perc[a];
-                    data.push(obj);
-                }
-                $('#res').jQCloud('update', data);
-            }
-        });
-        setTimeout(viewData, 5000);
+            });
+            setTimeout(viewData, 5000);
+        }
     }
     viewData();
 });
@@ -78,3 +81,8 @@ $("#save").click(function(){
         $("#alertModal").modal('hide');
     });
 });
+
+$("#closeEvent").click(function(){
+    stop=true;
+    $.notify("Event Closed!", "success");
+})
